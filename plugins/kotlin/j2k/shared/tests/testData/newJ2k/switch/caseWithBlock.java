741289390d8d9d0bdfdf072393e4cc46e9f12a14
Eve Matthaey <ermatt@meta.com>
2024-03-05 14:23:35 -0600

    Add test cases for comments on when expressions

--- plugins/kotlin/j2k/shared/tests/testData/newJ2k/switch/caseWithBlock.java
+++ plugins/kotlin/j2k/shared/tests/testData/newJ2k/switch/caseWithBlock.java
@@ -1,20 +1,20 @@
 //method
 void foo() {
   switch(a) {
-    case 1: {
+    /* case 1 */ case 1: {
       int x = 1;
       System.out.println(x);
       break;
     }
-
+    // case 2
     case 2: {
       int x = 2;
       System.out.println(x);
     }
     break;
 
-    case 3: {
+    case 3: { // case 3 top
         System.out.println(3);
-    }
+    } // case 3 bottom
   }
 }
--- plugins/kotlin/j2k/shared/tests/testData/newJ2k/switch/caseWithBlock.kt
+++ plugins/kotlin/j2k/shared/tests/testData/newJ2k/switch/caseWithBlock.kt
@@ -1,17 +1,17 @@
 fun foo() {
     when (a) {
-        1 -> {
+        /* case 1 */ 1 -> {
             val x = 1
             println(x)
         }
-
+        // case 2
         2 -> {
             val x = 2
             println(x)
         }
 
-        3 -> {
+        3 -> { // case 3 top
             println(3)
-        }
+        } // case 3 bottom
     }
 }
